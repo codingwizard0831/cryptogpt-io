73b5b5cf49f41278b1bc215164d712176d58bb49
CodeGenerator0117 <sy051569@gmail.com>
2024-06-05 05:36:03 -0700

    add endpoint signup, signin

---
+++ src/app/api/auth/signinwithpassword/route.ts
@@ -0,0 +1,14 @@
+import { supabase } from 'src/lib/supabase';
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const res = await req.json();
+    const email = res.email;
+    const password = res.password;
+    const response = await supabase.auth.signInWithPassword({ email, password })    
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ src/app/api/auth/signupwithpassword/route.ts
@@ -0,0 +1,15 @@
+import { supabase } from 'src/lib/supabase';
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const res = await req.json();
+    const email = res.email;
+    const password = res.password;
+    const response = await supabase.auth.signUp({ email, password })
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
