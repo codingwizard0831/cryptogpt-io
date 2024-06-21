f71df8bb0c8c210b32ede190f5525301641cc82e
yurii0117 <sy051569@gmail.com>
2024-06-21 08:29:12 -0700

    update bearer token

--- next.config.js
+++ next.config.js
@@ -21,13 +21,20 @@ module.exports = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: '/api/:path*', // replace '/api' with your actual API route
+        source: "/api/:path*",
         headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace '*' with your actual allowed origin
-          { key: 'Access-Control-Allow-Methods', value: 'GET, DELETE, PATCH, POST, PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
         ],
       },
     ];
