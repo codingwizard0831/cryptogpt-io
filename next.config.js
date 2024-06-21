3cef83d7ae98b833ac2c2975651c3b0cca571270
yurii0117 <sy051569@gmail.com>
2024-06-21 07:34:21 -0700

    configure CORS in next.config.js

--- next.config.js
+++ next.config.js
@@ -18,4 +18,18 @@ module.exports = {
     });
     return config;
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*', // replace '/api' with your actual API route
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace '*' with your actual allowed origin
+          { key: 'Access-Control-Allow-Methods', value: 'GET, DELETE, PATCH, POST, PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ];
+  },
 };
