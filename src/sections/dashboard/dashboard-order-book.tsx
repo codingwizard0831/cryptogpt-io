10e93acc1c07112e9846833847b2a1bf68e051f8
codingenchanter725 <codingenchanter725@gmail.com>
2024-06-25 08:51:56 -0700

    feat: SELL/BUY layout

--- src/sections/dashboard/dashboard-order-book.tsx
+++ src/sections/dashboard/dashboard-order-book.tsx
@@ -6,18 +6,21 @@ import { Box, Stack, Table, alpha, TableRow, TableHead, TableCell, TableBody, Ty
 
 import { fNumberPrice } from 'src/utils/format-number';
 
+import { MAIN_CHART_PANEL } from 'src/layouts/config-layout';
+
 import Iconify from 'src/components/iconify';
 
 export default function DashboardOrderBook() {
-    const [sellOrders, setSellOrders] = useState([...dummySellOrders]);
-    const [buyOrders, setBuyOrders] = useState([...dummyBuyOrders]);
+    const [sellOrders, setSellOrders] = useState([...dummySellOrders, ...dummySellOrders, ...dummySellOrders]);
+    const [buyOrders, setBuyOrders] = useState([...dummyBuyOrders, ...dummyBuyOrders, ...dummyBuyOrders]);
     const orderBookContainer = useRef<HTMLDivElement>(null);
-    const [currentSelectedSellOrder, setCurrentSelectedSellOrder] = useState(dummySellOrders.length - 1);
+    const [currentSelectedSellOrder, setCurrentSelectedSellOrder] = useState(sellOrders.length - 1);
     const [currentSelectedBuyOrder, setCurrentSelectedBuyOrder] = useState(0);
     const [averagePrice, setAveragePrice] = useState(0);
     const [sumBTC, setSumBTC] = useState(0);
     const [sumUSDT, setSumUSDT] = useState(0);
     const [topOfInfoModal, setTopOfInfoModal] = useState("calc(50% - 47px)");
+    const [buySellLayout, setBuySellLayout] = useState<"SELL" | "BUY" | "BOTH">("BOTH");
 
     useEffect(() => {
         let _averagePrice = 0;
@@ -58,14 +61,17 @@ export default function DashboardOrderBook() {
         <Box ref={orderBookContainer}>
             <Typography variant="h6">Order Book</Typography>
             <Stack direction="row" spacing={1} sx={{ my: 1 }}>
-                <Iconify icon="fluent:layout-column-two-split-left-focus-top-left-24-filled" sx={{
+                <Iconify icon="fluent:layout-column-two-split-left-focus-top-left-24-filled" onClick={() => setBuySellLayout("BOTH")} sx={{
                     cursor: 'pointer',
+                    color: buySellLayout === "BOTH" ? 'text.primary' : 'text.secondary',
                 }} />
-                <Iconify icon="fluent:layout-column-two-focus-left-24-filled" sx={{
+                <Iconify icon="fluent:layout-column-two-focus-left-24-filled" onClick={() => setBuySellLayout("BUY")} sx={{
                     cursor: 'pointer',
+                    color: buySellLayout === "BUY" ? 'success.main' : 'text.secondary',
                 }} />
-                <Iconify icon="fluent:layout-column-two-16-regular" sx={{
+                <Iconify icon="fluent:layout-column-two-16-regular" onClick={() => setBuySellLayout("SELL")} sx={{
                     cursor: 'pointer',
+                    color: buySellLayout === "SELL" ? 'error.main' : 'text.secondary',
                 }} />
             </Stack>
 
@@ -86,44 +92,73 @@ export default function DashboardOrderBook() {
             }}>
                 <TableHead>
                     <TableRow>
-                        <TableCell>
-                            <Typography variant="caption" width="30%">Price(USDT)</Typography>
+                        <TableCell width="30%">
+                            <Typography variant="caption">Price(USDT)</Typography>
                         </TableCell>
-                        <TableCell align='right'>
-                            <Typography variant="caption" width="35%">Amount(BTC)</Typography>
+                        <TableCell align='right' width="35%">
+                            <Typography variant="caption">Amount(BTC)</Typography>
                         </TableCell>
-                        <TableCell align='right'>
-                            <Typography variant="caption" width="35%">Total</Typography>
+                        <TableCell align='right' width="35%">
+                            <Typography variant="caption">Total</Typography>
                         </TableCell>
                     </TableRow>
                 </TableHead>
-                <TableBody>
-                    {
-                        dummySellOrders.map((_sellOrder, _index) => (
-                            <TableRow key={`row-key-${_index}`}
-                                onMouseEnter={(e) => handleMouseEnterSellOrder(e, _index)}
-                                onMouseLeave={() => setCurrentSelectedSellOrder(dummySellOrders.length - 1)}
-                                sx={{
-                                    backgroundColor: currentSelectedSellOrder <= _index ? theme => alpha(theme.palette.error.main, 0.1) : 'transparent',
-                                }}>
-                                <TableCell align='left' width="30%">
-                                    <Typography variant="caption" align='left' sx={{
-                                        color: theme => theme.palette.error.main,
-                                    }}>{fNumberPrice(_sellOrder.price, 2)}</Typography>
-                                </TableCell>
-                                <TableCell align='right' width="35%">
-                                    <Typography variant="caption" align="right">{fNumberPrice(_sellOrder.amount)}</Typography>
-                                </TableCell>
-                                <TableCell align='right' width="35%">
-                                    <Typography variant="caption" align="right">{fNumberPrice(_sellOrder.price * _sellOrder.amount)}</Typography>
-                                </TableCell>
-                            </TableRow>
-                        ))
-                    }
-                </TableBody>
             </Table>
 
-            <Stack direction="row" alignItems="center" sx={{ mt: 2 }}>
+            <Box sx={{
+                maxHeight: `${MAIN_CHART_PANEL.W_DESKTOP - 140}px`,
+                overflowY: 'auto',
+                overflowX: 'hidden',
+            }}>
+                <Table sx={{
+                    "& td,th": {
+                        fontSize: '10px',
+                        padding: '0px',
+                    },
+                    "& th": {
+                        backgroundColor: 'transparent',
+                    },
+                    "& tbody tr": {
+                        transition: 'background-color 0.3s',
+                        "&:hover": {
+                            backgroundColor: theme => alpha(theme.palette.background.opposite, 0.1)
+                        },
+                    },
+                }}>
+                    {
+                        buySellLayout !== "BUY" &&
+                        <TableBody>
+                            {
+                                (buySellLayout === "BOTH" ? sellOrders.filter((_, _i) => _i < 11) : sellOrders).map((_sellOrder, _index) => (
+                                    <TableRow key={`row-key-${_index}`}
+                                        onMouseEnter={(e) => handleMouseEnterSellOrder(e, _index)}
+                                        onMouseLeave={() => setCurrentSelectedSellOrder(dummySellOrders.length - 1)}
+                                        sx={{
+                                            backgroundColor: currentSelectedSellOrder <= _index ? theme => alpha(theme.palette.error.main, 0.1) : 'transparent',
+                                        }}>
+                                        <TableCell align='left' width="30%">
+                                            <Typography variant="caption" align='left' sx={{
+                                                color: theme => theme.palette.error.main,
+                                            }}>{fNumberPrice(_sellOrder.price, 2)}</Typography>
+                                        </TableCell>
+                                        <TableCell align='right' width="35%">
+                                            <Typography variant="caption" align="right">{fNumberPrice(_sellOrder.amount)}</Typography>
+                                        </TableCell>
+                                        <TableCell align='right' width="35%">
+                                            <Typography variant="caption" align="right">{fNumberPrice(_sellOrder.price * _sellOrder.amount)}</Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                ))
+                            }
+                        </TableBody>
+                    }
+                </Table>
+            </Box>
+
+            <Stack direction="row" alignItems="center" sx={{
+                mt: buySellLayout !== "BUY" ? 1 : 0,
+                mb: buySellLayout !== "SELL" ? 1 : 0,
+            }}>
                 <Typography variant="h6" color="error">61,076.93</Typography>
                 <Iconify icon="mingcute:arrow-down-fill" sx={{
                     color: theme => theme.palette.error.main,
@@ -132,69 +167,81 @@ export default function DashboardOrderBook() {
                 <Typography variant="caption" sx={{ ml: 2 }}>$61,076.93</Typography>
             </Stack>
 
-            <Table sx={{
-                "& td,th": {
-                    fontSize: '10px',
-                    padding: '0px',
-                },
-                "& tbody tr": {
-                    transition: 'background-color 0.3s',
-                    "&:hover": {
-                        backgroundColor: theme => alpha(theme.palette.background.opposite, 0.1)
-                    },
-                },
+            <Box sx={{
+                maxHeight: `${MAIN_CHART_PANEL.W_DESKTOP - 140}px`,
+                overflowY: 'auto',
+                overflowX: 'hidden',
             }}>
-                <TableBody>
-                    {
-                        dummyBuyOrders.map((_buyOrder, _index) => (
-                            <TableRow key={`row-key-${_index}`}
-                                onMouseEnter={(e) => handleMouseEnterBuyOrder(e, _index)}
-                                onMouseLeave={() => setCurrentSelectedBuyOrder(0)}
-                                sx={{
-                                    backgroundColor: currentSelectedBuyOrder >= _index ? theme => alpha(theme.palette.success.main, 0.1) : 'transparent',
-                                }}>
-                                <TableCell align='left' width="30%">
-                                    <Typography variant="caption" align='left' sx={{
-                                        color: theme => theme.palette.success.main,
-                                    }}>{fNumberPrice(_buyOrder.price, 2)}</Typography>
-                                </TableCell>
-                                <TableCell align='right' width="35%">
-                                    <Typography variant="caption" align="right">{fNumberPrice(_buyOrder.amount)}</Typography>
-                                </TableCell>
-                                <TableCell align='right' width="35%">
-                                    <Typography variant="caption" align="right">{fNumberPrice(_buyOrder.price * _buyOrder.amount)}</Typography>
-                                </TableCell>
-                            </TableRow>
-                        ))
-                    }
-                </TableBody>
-            </Table>
-
-            <Stack direction="row" alignItems="center" spacing={0.5} sx={{ mt: 2 }}>
-                <Typography variant="body2">B</Typography>
-                <Typography variant="caption" sx={{
-                    color: theme => theme.palette.success.main,
-                }}>32.34%</Typography>
-                <Stack direction="row" alignItems="center" spacing={0.5} sx={{
-                    borderRadius: '4px',
-                    flex: 1,
+                <Table sx={{
+                    "& td,th": {
+                        fontSize: '10px',
+                        padding: '0px',
+                    },
+                    "& tbody tr": {
+                        transition: 'background-color 0.3s',
+                        "&:hover": {
+                            backgroundColor: theme => alpha(theme.palette.background.opposite, 0.1)
+                        },
+                    },
                 }}>
-                    <Box sx={{
-                        width: '30%',
-                        height: '4px',
-                        backgroundColor: theme => theme.palette.success.main,
-                    }} />
-                    <Box sx={{
-                        flexGrow: 1,
-                        height: '4px',
-                        backgroundColor: theme => theme.palette.error.main,
-                    }} />
+                    {
+                        buySellLayout !== "SELL" &&
+                        <TableBody>
+                            {
+                                (buySellLayout === "BOTH" ? buyOrders.filter((_, _i) => _i > buyOrders.length - 11) : buyOrders).map((_buyOrder, _index) => (
+                                    <TableRow key={`row-key-${_index}`}
+                                        onMouseEnter={(e) => handleMouseEnterBuyOrder(e, _index)}
+                                        onMouseLeave={() => setCurrentSelectedBuyOrder(0)}
+                                        sx={{
+                                            backgroundColor: currentSelectedBuyOrder >= _index ? theme => alpha(theme.palette.success.main, 0.1) : 'transparent',
+                                        }}>
+                                        <TableCell align='left' width="30%">
+                                            <Typography variant="caption" align='left' sx={{
+                                                color: theme => theme.palette.success.main,
+                                            }}>{fNumberPrice(_buyOrder.price, 2)}</Typography>
+                                        </TableCell>
+                                        <TableCell align='right' width="35%">
+                                            <Typography variant="caption" align="right">{fNumberPrice(_buyOrder.amount)}</Typography>
+                                        </TableCell>
+                                        <TableCell align='right' width="35%">
+                                            <Typography variant="caption" align="right">{fNumberPrice(_buyOrder.price * _buyOrder.amount)}</Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                ))
+                            }
+                        </TableBody>
+                    }
+                </Table>
+            </Box>
+
+            {
+                buySellLayout === "BOTH" &&
+                <Stack direction="row" alignItems="center" spacing={0.5} sx={{ mt: 1 }}>
+                    <Typography variant="body2">B</Typography>
+                    <Typography variant="caption" sx={{
+                        color: theme => theme.palette.success.main,
+                    }}>32.34%</Typography>
+                    <Stack direction="row" alignItems="center" spacing={0.5} sx={{
+                        borderRadius: '4px',
+                        flex: 1,
+                    }}>
+                        <Box sx={{
+                            width: '30%',
+                            height: '4px',
+                            backgroundColor: theme => theme.palette.success.main,
+                        }} />
+                        <Box sx={{
+                            flexGrow: 1,
+                            height: '4px',
+                            backgroundColor: theme => theme.palette.error.main,
+                        }} />
+                    </Stack>
+                    <Typography variant="caption" sx={{
+                        color: theme => theme.palette.error.main,
+                    }}>{(100 - 32.34).toFixed(2)}%</Typography>
+                    <Typography variant="body2">S</Typography>
                 </Stack>
-                <Typography variant="caption" sx={{
-                    color: theme => theme.palette.error.main,
-                }}>{(100 - 32.34).toFixed(2)}%</Typography>
-                <Typography variant="body2">S</Typography>
-            </Stack>
+            }
 
             <Box sx={{
                 width: '240px',
