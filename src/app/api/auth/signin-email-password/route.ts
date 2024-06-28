7f604465020e0b044a3d0ae390a25ddbb17c33d6
yurii0117 <sy051569@gmail.com>
2024-06-28 05:41:57 -0700

    update signin & signup endpoints for email-password, token login

--- src/app/api/auth/signin-email-password/route.ts
+++ src/app/api/auth/signin-email-password/route.ts
@@ -8,6 +8,26 @@ export async function POST(req: Request) {
     const { email } = res;
     const { password } = res;
     const response = await supabase.auth.signInWithPassword({ email, password })
+    console.log('response', response)
+    const userId = response?.data?.user?.id;
+    if (!userId) {
+      return NextResponse.json({ error: 'Failed to sign in' }, { status: 500 })
+    }
+    const { error: upsertError } = await supabase
+      .from('users')
+      .update([
+        {
+          auth: {
+            lastLoggedinTime: new Date().toISOString(),
+            lastAuthStatus: "success",
+            lastLoggedinProvider: "email"
+          }
+        }
+      ])
+      .eq('userId', userId)
+    if (upsertError) {
+      throw new Error("Failed to put data to users table")
+    }
     return NextResponse.json(response)
   } catch (error) {
     console.log(error);
