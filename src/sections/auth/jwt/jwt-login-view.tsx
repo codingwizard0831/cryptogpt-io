e68f0005c1a9ea8a029ec03a5edbeaf958f9a10c
goldstar105000117 <goldstar105000117@gmail.com>
2024-08-13 02:07:54 -0700

    fix: fix google oauth issue

--- src/sections/auth/jwt/jwt-login-view.tsx
+++ src/sections/auth/jwt/jwt-login-view.tsx
@@ -194,7 +194,7 @@ export default function JwtLoginView() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/auth/jwt/supabase-oauth-callback1`
+        redirectTo: `${window.location.origin}/auth/jwt/supabase-oauth-callback1?`,
       },
     });
   }
