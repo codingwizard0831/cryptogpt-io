1a5a5bce6a35beb9eed9e839380c8d1677beb67e
codingenchanter725 <codingenchanter725@gmail.com>
2024-08-16 12:06:21 +0300

    feat: 2d part for strategy

---
+++ src/app/dashboard/strategy/beta/page.tsx
@@ -0,0 +1,11 @@
+import { DashboardStrategy3dView } from 'src/sections/strategy/view';
+
+// ----------------------------------------------------------------------
+
+export const metadata = {
+    title: 'Dashboard: Strategy beta 3D',
+};
+
+export default function Page() {
+    return <DashboardStrategy3dView />;
+}
--- src/sections/strategy/view/index.ts
+++ src/sections/strategy/view/index.ts
@@ -1,1 +1,2 @@
 export { default as DashboardStrategyView } from "./dashboard-strategy-view";
+export { default as DashboardStrategy3dView } from "./dashboard-strategy-3d-view";
