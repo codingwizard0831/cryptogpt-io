530574fb1b44ad0938e87936377be19e3ae0c0ec
goldstar105000117 <goldstar105000117@gmail.com>
2024-08-25 23:50:38 -0700

    feat: add apple face id backend

---
+++ src/app/api/auth/webauthn-register/route.ts
@@ -0,0 +1,67 @@
+import { NextResponse } from "next/server";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { verifyRegistrationResponse, generateRegistrationOptions } from '@simplewebauthn/server';
+
+import { supabase } from "src/lib/supabase";
+
+const rpName = 'cryptogpt';
+const rpID = 'cryptogpt.app';
+const origin = `https://${rpID}`;
+
+export async function POST(req: Request) {
+  const options: any = generateRegistrationOptions({
+    rpName,
+    rpID,
+    userID: "50df3478-f7ae-4d41-b4fb-bd10ee613a05",
+    userName: "goldstar105000117@gmail.com",
+    attestationType: 'none',
+    authenticatorSelection: {
+      authenticatorAttachment: 'platform',
+      userVerification: 'required',
+    },
+  });
+
+  await supabase.from('webauthn_challenges').insert({
+    user_id: "50df3478-f7ae-4d41-b4fb-bd10ee613a05",
+    challenge: options.challenge,
+  });
+
+  return NextResponse.json({ success: true, data: options })
+}
+
+export async function PUT(req: Request) {
+  const res = await req.json();
+  const { attestationResponse } = res;
+
+  const { data: challengeData }: any = await supabase
+    .from('webauthn_challenges')
+    .select('challenge')
+    .eq('user_id', "50df3478-f7ae-4d41-b4fb-bd10ee613a05")
+    .order('created_at', { ascending: false })
+    .limit(1)
+    .single();
+
+  const expectedChallenge = challengeData.challenge;
+
+  const verification: any = await verifyRegistrationResponse({
+    response: attestationResponse,
+    expectedChallenge: `${expectedChallenge}`,
+    expectedOrigin: origin,
+    expectedRPID: rpID,
+  });
+
+  if (verification.verified) {
+    const { credentialID, credentialPublicKey }: any = verification.registrationInfo;
+
+    await supabase.from('webauthn_credentials').insert({
+      user_id: "50df3478-f7ae-4d41-b4fb-bd10ee613a05",
+      credential_id: Buffer.from(credentialID).toString('base64'),
+      public_key: Buffer.from(credentialPublicKey).toString('base64'),
+      counter: verification.registrationInfo.counter,
+    });
+
+    return NextResponse.json({ success: true })
+  }
+
+  return NextResponse.json({ success: false, error: "Registration failed" }, { status: 400 })
+}
