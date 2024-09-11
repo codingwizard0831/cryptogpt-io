f2c7339a157c33431ae66a0ef65184891b795e55
goldstar105000117 <goldstar105000117@gmail.com>
2024-09-11 04:12:28 -0700

    feat: add transaction section

--- src/app/api/history/crgpt-token/route.ts
+++ src/app/api/history/crgpt-token/route.ts
@@ -27,3 +27,29 @@ export async function GET(req: NextRequest) {
     );
   }
 }
+
+export async function POST(req: NextRequest) {
+  const supabase = createCustomServerClient();
+  const userHeader = req.headers.get("x-user") as string;
+
+  if (!userHeader) {
+    return NextResponse.json({ success: false, error: "User not authenticated" }, { status: 401 });
+  }
+  const user = JSON.parse(userHeader);
+  console.log("userId", user)
+  try {
+    const { data, error } = await supabase.from("user_crgpt_token_history").select("*").eq("user_id", user?.id).eq("status", "admin_waiting").order("created_at", {ascending: false});
+    if (error)
+      return NextResponse.json(
+        { code: error.code, error: error.message },
+        { status: 500 }
+      );
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error fetching user CRGPT token history:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch history data" },
+      { status: 500 }
+    );
+  }
+}
