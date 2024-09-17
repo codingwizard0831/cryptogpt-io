2679a04f30716b4cb4e64be5f83101c8d115ed9b
goldstar105000117 <goldstar105000117@gmail.com>
2024-09-17 11:22:15 -0700

    update: get languages from db

--- src/layouts/common/language-popover.tsx
+++ src/layouts/common/language-popover.tsx
@@ -1,8 +1,11 @@
 import { m } from 'framer-motion';
-import { useCallback } from 'react';
+import { useMemo, useState, useEffect, useCallback } from 'react';
 
 import MenuItem from '@mui/material/MenuItem';
 import IconButton from '@mui/material/IconButton';
+
+import { flags } from 'src/assets/data';
+import { getUserProfileInfo } from 'src/auth/context/jwt/utils';
 
 import Iconify from 'src/components/iconify';
 import { varHover } from 'src/components/animate';
@@ -10,46 +13,26 @@ import CustomPopover, { usePopover } from 'src/components/custom-popover';
 
 // ----------------------------------------------------------------------
 
-export const allLangs = [
-  {
-    label: 'English',
-    value: 'en',
-    icon: 'flagpack:gb-nir',
-  },
-  {
-    label: 'French',
-    value: 'fr',
-    icon: 'flagpack:fr',
-  },
-  {
-    label: 'Vietnamese',
-    value: 'vi',
-    icon: 'flagpack:vn',
-  },
-  {
-    label: 'Chinese',
-    value: 'cn',
-    icon: 'flagpack:cn',
-  },
-  {
-    label: 'Arabic',
-    value: 'ar',
-    icon: 'flagpack:sa',
-  },
-];
-
 export default function LanguagePopover() {
+  const user_profile = getUserProfileInfo();
+  const allLangs: any = useMemo(() => user_profile?.languages || [], [user_profile?.languages]);
   const popover = usePopover();
+  const [currentLang, setCurrentLang] = useState<any>([]);
 
-  const currentLang = allLangs[0];
+  useEffect(() => {
+    if (allLangs.length > 0) {
+      setCurrentLang(allLangs[0]);
+    }
+  }, [allLangs]);
 
-  const handleChangeLang = useCallback(() => {
+  const handleChangeLang = useCallback((lang: typeof allLangs[0]) => {
+    setCurrentLang(lang);
     popover.onClose();
   }, [popover]);
 
   return (
     <>
-      <IconButton
+      {currentLang?.code && <IconButton
         component={m.button}
         whileTap="tap"
         whileHover="hover"
@@ -63,19 +46,18 @@ export default function LanguagePopover() {
           }),
         }}
       >
-        <Iconify icon={currentLang.icon} sx={{ borderRadius: 0.65, width: 28 }} />
-      </IconButton>
+        <Iconify icon={flags[currentLang?.code].icon} sx={{ borderRadius: 0.65, width: 28 }} />
+      </IconButton>}
 
       <CustomPopover open={popover.open} onClose={popover.onClose} sx={{ width: 160 }}>
-        {allLangs.map((option) => (
+        {allLangs?.length > 0 && allLangs?.map((option) => (
           <MenuItem
-            key={option.value}
-            selected={option.value === currentLang.value}
-            onClick={handleChangeLang}
+            key={option.code}
+            selected={option.code === currentLang?.code}
+            onClick={() => handleChangeLang(option)}
           >
-            <Iconify icon={option.icon} sx={{ borderRadius: 0.65, width: 28 }} />
-
-            {option.label}
+            <Iconify icon={flags[option.code].icon} sx={{ borderRadius: 0.65, width: 28, mr: 1 }} />
+            {flags[option.code].name}
           </MenuItem>
         ))}
       </CustomPopover>
