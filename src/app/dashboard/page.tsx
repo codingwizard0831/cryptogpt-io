b170e658bd555a0bfed22f26aef887cd139aaa29
codingenchanter725 <codingenchanter725@gmail.com>
2024-09-17 17:30:01 +0300

    update: fix the metadata for SEO

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { DashboardView } from 'src/sections/overview/view';
 // ----------------------------------------------------------------------
 
 export const metadata = {
-  title: 'Dashboard: Chat',
+  title: 'Dashboard: Overview',
 };
 
 export default function Page() {
