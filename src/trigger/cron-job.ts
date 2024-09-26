d70d7adf32844dc9b554bff4586df4b04211ea37
goldstar105000117 <goldstar105000117@gmail.com>
2024-09-25 23:49:06 -0700

    update: implement cron job

--- src/trigger/cron-job.ts
+++ src/trigger/cron-job.ts
@@ -3,20 +3,14 @@ import fetch from 'node-fetch';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { schedules } from "@trigger.dev/sdk/v3";
 
-// Define the task
 export const callEndpointTask = schedules.task({
   id: "call-endpoint-every-5-seconds",
-  cron: "* * * * *",
+  cron: "*/5 * * * *",
   run: async () => {
     try {
-      // Make the API call
-      const response = await fetch('https://cryptogpt.app/api/auth/token/', {
+      await fetch('https://cryptogpt.app/api/auth/transaction/', {
         method: 'POST',
-        body: JSON.stringify({ token: "1234567890" }),
       });
-
-      const data = await response.json();
-      console.log("API Response:", data);
     } catch (error) {
       console.error("Error calling API:", error);
     }
